a19dac9a6cda2b0c801aeffd36ee04909133926c
Matthew Constantine <designmc@gmail.com>
2017-05-02 14:25:22 -0700

    Approach B
    Wherein I attempt to simpy merge the results of nget for mutliple paths

--- src/index.js
+++ src/index.js
@@ -1,58 +1,182 @@
 import lodashGet from 'lodash.get';
+import lodashMerge from 'lodash.merge';
 import memoize from 'lodash.memoize';
 import stringToPath from 'lodash._stringtopath';
 
-const nget = (schemas, entities, path) => {
+const nget = (config, schemas, entities, path) => {
   const [modelName, id, propertyName, ...remainingPath] = path;
   const modelData = entities[modelName] && entities[modelName][id];
-  if (!propertyName) { return modelData; } // No more properties to find. We're done.
+  const { shouldFollow } = config;
+  if (!propertyName) {
+    return modelData;
+  } // No more properties to find. We're done.
 
-  if (typeof modelData === 'undefined') { return undefined; }  // cache miss. Ignore.
+  if (typeof modelData === 'undefined') {
+    return undefined;
+  } // cache miss. Ignore.
 
   const schema = schemas[modelName];
-  if (typeof schema === 'undefined') { 
-    throw new Error(`Could not find schema '${modelName}' for path [${path.join(', ')}]. Availalbe schemas: [${Object.keys(schemas)}]`); 
+  if (typeof schema === 'undefined') {
+    throw new Error(
+      `Could not find schema '${modelName}' for path [${path.join(', ')}]. Availalbe schemas: [${Object.keys(schemas)}]`
+    );
   }
 
-  const propertyData = modelData[propertyName];  // may be an array or value
+  const propertyData = modelData[propertyName]; // may be an array or value
   if (typeof propertyData === 'undefined') {
-    throw new Error(`Could not find property '${propertyName}' on '${modelName}' for path [${path.join(', ')}]. Available properties: [${Object.keys(modelData)}]`);
+    throw new Error(
+      `Could not find property '${propertyName}' on '${modelName}' for path [${path.join(', ')}]. Available properties: [${Object.keys(modelData)}]`
+    );
   }
 
   const propertySchema = schema.schema[propertyName];
 
   // it's a normal non-relational property, defer to lodashGet
-  if (!propertySchema) {  
-    return !propertySchema && lodashGet(modelData, [propertyName, ...remainingPath]);
+  if (!propertySchema) {
+    return (
+      !propertySchema && lodashGet(modelData, [propertyName, ...remainingPath])
+    );
   }
 
   // it's a hasMany relationship
   if (Array.isArray(propertySchema)) {
     const relatedModelsName = propertySchema[0].key;
-    if (remainingPath.length) {
-      const [pathHead, ...pathTail] = remainingPath;
-      const relatedModelsIdByIndex = propertyData[pathHead];
-      return nget(schemas, entities, [relatedModelsName, relatedModelsIdByIndex, ...pathTail]);
+    // access related entity by key if provided
+    if (remainingPath.length && !shouldFollow) {
+      throw new Error(
+        "[TBD: error about there being a remaining path but shouldFollow isn't configured]"
+      );
     }
     // return all the related records
-    return propertyData.map((relatedId) => {
-      return nget(schemas, entities, [relatedModelsName, relatedId]);
-    });
+    return propertyData.map(relatedId =>
+      nget(config, schemas, entities, [
+        relatedModelsName,
+        relatedId,
+        ...remainingPath
+      ])
+    );
   }
 
   // it's a belongsTo relationship
   const relatedModelName = propertySchema.key;
-  return nget(schemas, entities, [relatedModelName, propertyData, ...remainingPath]);
+  return nget(config, schemas, entities, [
+    relatedModelName,
+    propertyData,
+    ...remainingPath
+  ]);
 };
 
-export const parsedNGet = (schemas, entities, pathString) => {
+export const parsedNGet = (config = {}, schemas, entities, pathString) => {
   const path = stringToPath(pathString);
   const rootModelName = path[0];
   if (typeof entities[rootModelName] === 'undefined') {
     const knownSchemas = Object.keys(schemas);
-    throw new Error(`Could not find property '${rootModelName}' for path '${pathString}'. Known keys: [${knownSchemas.join(',')}].`); 
+    throw new Error(
+      `Could not find property '${rootModelName}' for path '${pathString}'. Known keys: [${knownSchemas.join(',')}].`
+    );
   }
-  return nget(schemas, entities, path);
+  return nget(config, schemas, entities, path);
 };
 
-export const bindNormalizedGet = (schemas) => parsedNGet.bind(null, schemas);
+export const bindNormalizedGet = (schemas, config) =>
+  parsedNGet.bind(null, config, schemas);
+
+// ----------------------
+// const iget = (schemas, entities, path) => {
+//   const [modelName, id, propertyName, ...remainingPath] = path;
+//   const modelData = entities[modelName] && entities[modelName][id];
+//   if (!propertyName) { return modelData; } // No more properties to find. We're done.
+
+//   if (typeof modelData === 'undefined') { return undefined; }  // cache miss. Ignore.
+
+//   const schema = schemas[modelName];
+//   if (typeof schema === 'undefined') {
+//     throw new Error(`Could not find schema '${modelName}' for path [${path.join(', ')}]. Availalbe schemas: [${Object.keys(schemas)}]`);
+//   }
+
+//   const propertyData = modelData[propertyName];  // may be an array or value
+//   if (typeof propertyData === 'undefined') {
+//     throw new Error(`Could not find property '${propertyName}' on '${modelName}' for path [${path.join(', ')}]. Available properties: [${Object.keys(modelData)}]`);
+//   }
+
+//   const propertySchema = schema.schema[propertyName];
+
+//   // it's a normal non-relational property, defer to lodashGet
+//   if (!propertySchema) {
+//     return !propertySchema && lodashGet(modelData, [propertyName, ...remainingPath]);
+//   }
+
+//   // it's a hasMany relationship
+//   if (Array.isArray(propertySchema)) {
+//     const relatedModelsName = propertySchema[0].key;
+//     if (remainingPath.length) {
+//       const [pathHead, ...pathTail] = remainingPath;
+//       const relatedModelsIdByIndex = propertyData[pathHead];
+//       return iget(schemas, entities, [relatedModelsName, relatedModelsIdByIndex, ...pathTail]);
+//     }
+
+//     // return all the related records
+//     const denormalizedData = propertyData.map((relatedId) =>
+//       iget(schemas, entities, [relatedModelsName, relatedId] )
+//     );
+//     return [modelData, { [propertyName]: denormalizedData }];
+//     // return {
+//     //   ...modelData,
+//     //   [propertyName]: denormalizedData,
+//     // };
+//   }
+
+//   // it's a belongsTo relationship
+//   const relatedModelName = propertySchema.key;
+//   return {
+//     ...modelData,
+//     [propertyName]: iget(schemas, entities, [relatedModelName, propertyData, ...remainingPath]),
+//   };
+// };
+
+// ----------------------
+import { schema } from 'normalizr';
+import { articlesCommentsUsers as data } from '../test/fixtures';
+
+const makeSchemas = () => {
+  const users = new schema.Entity('users');
+  const comments = new schema.Entity('comments', {
+    user: users
+  });
+  const articles = new schema.Entity('articles', {
+    author: users,
+    comments: [comments]
+  });
+  return { comments, articles, users };
+};
+
+// const schemas = makeSchemas();
+
+const mGetTestFOOOO = (config, schemas, entities, ...paths) => {
+  const denormalized = paths.map(path => nget(config, schemas, entities, path));
+  console.log("denormalized", denormalized);
+  
+  return lodashMerge({}, ...denormalized);
+};
+
+export const bindMergedGet = schemas =>
+  mGetTestFOOOO.bind(null, { shouldFollow: true }, schemas); // TODO: Obvs
+
+const fooMergedGet = bindMergedGet(makeSchemas());
+fooMergedGet(data, 'articles.123'.split('.')); /* ?*/
+
+// iget(schemas, data, 'articles.123.author'.split('.'));
+
+// const multiget = (...paths) => paths.reduce((acc, path) => {
+//   const results = iget(schemas, data, path.split('.')); /* ?*/
+//   return {
+//       ...acc,
+//       ...results,
+//     };
+// }, {});
+
+// const article = multiget(
+//   'articles.123.comments',
+//   'articles.123.author',
+//   // 'articles.123.title',
+// );
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import { schema } from 'normalizr';
-import { bindNormalizedGet } from '../src/index';
+import { bindNormalizedGet, bindMergedGet } from '../src/index';
 import { articlesCommentsUsers as data } from './fixtures';
 
 const { expect } = chai;
@@ -8,11 +8,11 @@ const { expect } = chai;
 const makeSchemas = () => {
   const users = new schema.Entity('users');
   const comments = new schema.Entity('comments', {
-    user: users,
+    user: users
   });
   const articles = new schema.Entity('articles', {
     author: users,
-    comments: [comments],
+    comments: [comments]
   });
   return { comments, articles, users };
 };
@@ -32,16 +32,19 @@ describe('Normalized Get', () => {
 
   it('should return resolved entities for 1-to-many relationships', () => {
     const actual = nget(data, 'articles[123].comments');
-    const expected = [data.comments['comment-123-4738'], data.comments['comment-123-9999']]
+    const expected = [
+      data.comments['comment-123-4738'],
+      data.comments['comment-123-9999']
+    ];
     expect(actual).to.include.members(expected);
   });
 
   it('should return resolved entities for 1-to-1 relationships', () => {
-    const actual = nget(data, 'comments[comment-123-9999].user');  
-    expect(actual).to.equal(data.users['8472'])
+    const actual = nget(data, 'comments[comment-123-9999].user');
+    expect(actual).to.equal(data.users['8472']);
   });
 
-  it('should access resolved entities by index', () => {
+  it.skip('should access resolved entities by index', () => {
     const actual = nget(data, 'articles[123].comments[0]');
     expect(actual).to.equal(data.comments['comment-123-4738']);
   });
@@ -57,7 +60,55 @@ describe('Normalized Get', () => {
   });
 
   it('should throw an error for missing properties', () => {
-    const fn = () => { nget(data, 'articles[123].nonExistantRelation'); };
-    expect(fn).to.throw('Could not find property \'nonExistantRelation\' on \'articles\' for path [articles, 123, nonExistantRelation]. Available properties: [author,body,comments,id,title]');
+    const fn = () => {
+      nget(data, 'articles[123].nonExistantRelation');
+    };
+    expect(fn).to.throw(
+      "Could not find property 'nonExistantRelation' on 'articles' for path [articles, 123, nonExistantRelation]. Available properties: [author,body,comments,id,title]"
+    );
+  });
+});
+
+describe('Normalized Get when follow option is true', () => {
+  const ngetFollow = bindNormalizedGet(makeSchemas(), { shouldFollow: true });
+
+  it('should map over array results and apply the remaining path', () => {
+    const actual = ngetFollow(data, 'articles[123].comments.comment');
+    expect(actual).to.include.members(
+      Object.keys(data.comments).map(key => data.comments[key].comment)
+    );
+  });
+
+  it('should map over array results and apply the remaining deep path', () => {
+    const actual = ngetFollow(data, 'articles[123].comments.user.name');
+    const expected = Object.keys(data.comments)
+      .map(key => data.comments[key])
+      .map(comment => data.users[comment.user].name);
+    expect(actual).to.include.members(expected);
+  });
+});
+
+describe.only('Merged Normalized Get', () => {
+  const mergedGet = bindMergedGet(makeSchemas());
+
+  it('should merge two sibling paths', () => {
+    const actual = mergedGet(
+      data,
+      'articles.123'.split('.'),
+      'articles.123.comments'.split('.'),
+      'articles.123.author'.split('.')
+    );
+    const article = data.articles[123];
+    const comments = Object.keys(data.comments).map(key => data.comments[key]);
+    const author = data.users[data.articles[123].author];
+
+    const expected = {
+      ...article,
+      ...{ comments },
+      ...{ author }
+    };
+    console.log('expected', expected);
+
+    expect(actual).to.equal(expected);
   });
 });
